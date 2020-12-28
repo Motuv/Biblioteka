ab55dd4335c8cdfe159e9526fb11f5635910a528
Motuv <piotrmiroslaw1@gmail.com>
2020-12-28 12:14:46 +0100

    Add files via upload
    Connection with db

---
+++ przeglad ksiazek.php
@@ -0,0 +1,48 @@
+<?php
+
+$servername = "mysql.agh.edu.pl";
+$login = "piotrmir";
+$pass = "3gsp7M61TP3wYKDV";
+$dbname = "piotrmir";
+
+$conn = new mysqli($servername, $login, $pass, $dbname);
+
+if (mysqli_error($conn)) {
+    die("Connection failed: ".mysqli_error($conn));
+}
+
+$sql = "SELECT * FROM ksiazki";
+if($result = mysqli_query($conn, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>tytul</th>";
+                echo "<th>id_autora</th>";
+                echo "<th>gatunek</th>";
+                echo "<th>wydawnictwo</th>";
+                echo "<th>liczba wypozyczen</th>";
+                echo "<th>status</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id_ksiazki'] . "</td>";
+                echo "<td>" . $row['tytul'] . "</td>";
+                echo "<td>" . $row['id_autora'] . "</td>";
+                echo "<td>" . $row['gatunek'] . "</td>";
+                echo "<td>" . $row['wydawnictwo'] . "</td>";
+                echo "<td>" . $row['l_wypozyczen'] . "</td>";
+                echo "<td>" . $row['status'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+ 
+mysqli_close($conn);
+?>
